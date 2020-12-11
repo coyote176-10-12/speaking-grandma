834faa7a31fe9f849ce0305832d3a3d4603fd3f7
Javier <coyote176@msn.com>
2020-12-11 14:00:42 -0700

    commit tree f70356a1e3fd941b0d62a55f5937a8bb654bb14e

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(phrase)
     elsif
          phrase != phrase.upcase 
          return "HUH?! SPEAK UP, SONNY!"
-    #elsif 
+    elsif 
         phrase == "WHAT DID YOU EAT TODAY?"
         return "NO, NOT SINCE 1938!"
     else 
